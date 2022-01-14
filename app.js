43038eece3407d2efa855d4786cd5436e6bcdc61
nils-e13 <nils@sup2o.com>
2022-01-14 16:58:34 +0100

    das noch nicht fertig

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// 
+/*// 
 // INSERT INTO index.html: <script src="https://unpkg.com/ml5@latest/dist/ml5.min.js" type="text/javascript"></script>
 
 // Teachable Machine
@@ -69,4 +69,48 @@ function gotResults(error, results) {
   // Store the label and classify again!
   label = results[0].label;
   classifyVideo();
+}
+*/
+
+  // Global variable to store the classifier
+let classifier;
+
+// Label
+let label = 'listening...';
+
+// Teachable Machine model URL:
+let soundModel = 'https://teachablemachine.withgoogle.com/models/wuyd_Zs2K/';
+
+
+function preload() {
+  // Load the model
+  classifier = ml5.soundClassifier(soundModel + 'model.json');
+}
+
+function setup() {
+  createCanvas(320, 240);
+  // Start classifying
+  // The sound model will continuously listen to the microphone
+  classifier.classify(gotResult);
+}
+
+function draw() {
+  background(0);
+  // Draw the label in the canvas
+  fill(255);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text(label, width / 2, height / 2);
+}
+
+
+// The model recognizing a sound will trigger this event
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
 }
